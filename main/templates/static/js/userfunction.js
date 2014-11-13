fe9b3691c600aae0845194e491c76c11b18261f7
chao <chao9420@gmail.com>
2014-11-13 19:10:50 +0800

    Test logout api and add userfunction.js

---
+++ main/templates/static/js/userfunction.js
@@ -0,0 +1,31 @@
+/*
+*user常用函数
+*需同时引入mineclassapi.js跟jquery才能使用
+*/
+
+/*实现点击头像弹出菜单*/
+function showOverflow()
+  {
+   var oBtn=document.getElementById('head-avatar');
+    var oDiv=document.getElementById('action-overflow');
+ oBtn.onclick=function(ev)
+ { var oEvent=ev||event;
+   if(oDiv.style.display=='block'){
+        oDiv.style.display='none';
+   }else{ 
+   oDiv.style.display='block';
+   oEvent.cancelBubble=true;
+    }
+}              //取消冒泡
+ document.onclick=function()
+ {oDiv.style.display='none';} 
+ }
+
+//退出
+function Logout(){
+    api.user.Logout(function(result){
+      var parsedJson = jQuery.parseJSON(results);
+      if(parsedJson.code==1)window.location.href="home_page";
+      else alert("似乎出了点问题，你可以反馈给我们。");
+    });
+}
