888cad8e07f140ba26e0d8b5b0403967b797af10
HeapOverflow <sdspeed@sina.com>
2014-11-24 00:47:06 +0800

    debug+1

--- main/templates/static/umeditor/umeditor.config.js
+++ main/templates/static/umeditor/umeditor.config.js
@@ -133,10 +133,10 @@
     window.UMEDITOR_CONFIG = {
 
         //为编辑器实例添加一个路径，这个不能被注释
-        UMEDITOR_HOME_URL : ""
+        UMEDITOR_HOME_URL : "/static/umeditor/"
 
         //图片上传配置区
-        ,imageUrl:URL+"api/upload"             //图片上传提交地址
+        ,imageUrl:"api/upload"             //图片上传提交地址
         //,imagePath:URL + "php/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
         ,imageFieldName:"file"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
 
