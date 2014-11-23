addc8e6fee213904eb1987f0bad7d5b468112382
HeapOverflow <sdspeed@sina.com>
2014-11-24 00:40:13 +0800

    Fix UEditor

--- main/templates/static/umeditor/umeditor.config.js
+++ main/templates/static/umeditor/umeditor.config.js
@@ -133,12 +133,12 @@
     window.UMEDITOR_CONFIG = {
 
         //为编辑器实例添加一个路径，这个不能被注释
-        UMEDITOR_HOME_URL : URL
+        UMEDITOR_HOME_URL : "/static/umeditor/"
 
         //图片上传配置区
         ,imageUrl:URL+"api/upload"             //图片上传提交地址
         //,imagePath:URL + "php/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
-        //,imageFieldName:"file"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
+        ,imageFieldName:"file"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
 
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
